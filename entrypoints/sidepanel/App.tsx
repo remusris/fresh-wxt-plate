d37b6b442df7e6d1d232128513ccaa3a5521811f
remusris <91991249+remusris@users.noreply.github.com>
2024-12-02 19:28:47 -0700

    Working

--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div style={{ height: '500px', width: '500px' }} data-registry="plate">
+    <div className="h-full w-full" data-registry="plate">
       <PlateEditor />
     </div>
   );
