3a2d44cd386e3a89dbb612260055cdb4c56e70c2
remusris <remus.risnoveanu@gmail.com>
2024-12-03 01:30:31 -0700

    got the messaging api working

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,5 +1,15 @@
+import { onMessage } from '@/messaging/messaging';
+
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+
+  onMessage('getStringLength', (messageInput) => {
+    console.log('Received message:', messageInput);
+    const { id, data, type, timestamp, sender } = messageInput
+    return data.length
+
+  });
+
 
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true})
 });
---
+++ messaging/messaging.ts
@@ -0,0 +1,7 @@
+import { defineExtensionMessaging } from '@webext-core/messaging';
+
+interface ProtocolMap {
+  getStringLength(data: string): number;
+}
+
+export const { sendMessage, onMessage } = defineExtensionMessaging<ProtocolMap>();
