f879c4ea1d0de782dbd6b55e51dadad9596184fc
remusris <remus.risnoveanu@gmail.com>
2024-12-03 12:40:22 -0700

    Update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'wxt';
 
+import { defineRunnerConfig } from 'wxt';
+
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permission: ["activeTab", "scripting", "sidePanel", "storage", "tabs"]
-  }
+  },
+  runner: defineRunnerConfig({
+    disabled: true
+  })
+
 });
+
+
