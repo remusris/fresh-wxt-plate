5bdb1201c225939e2506201ed72638c690a259cc
remusris <remus.risnoveanu@gmail.com>
2024-12-16 23:50:43 -0700

    Update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'wxt';
-
 import { defineRunnerConfig } from 'wxt';
-
 import commands from './modules/commands';
-
 // modules: ['@wxt-dev/module-react', commands],
 
 
@@ -22,3 +19,43 @@ export default defineConfig({
 });
 
 
+
+
+
+// See https://wxt.dev/api/config.html
+/* export default defineConfig({
+  extensionApi: 'chrome',
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    permissions: ["activeTab", "scripting", "sidePanel", "storage", "tabs", "history", "unlimitedStorage", "commands"],
+    description: "A browser extension that allows you to view and interact with your browser history in a side panel.",
+    commands: {
+      "_execute_action": {
+        suggested_key: {
+          default: "Alt+Q",
+          mac: "Alt+Q",
+          windows: "Alt+Q",
+          linux: "Alt+Q"
+        },
+        description: "Open the side panel."
+      },
+      "random-command": {
+        suggested_key: {
+          default: "Ctrl+Shift+Y",
+          mac: "Command+Shift+Y",
+          windows: "Ctrl+Shift+Y",
+          linux: "Ctrl+Shift+Y"
+        },
+        description: "Open the side panel."
+      }
+    },
+    action: {
+      default_title: "Open Side Panel",
+    },
+  },
+  runner: defineRunnerConfig({
+    disabled: true
+  }),
+  manifestVersion: 3
+});
+ */
