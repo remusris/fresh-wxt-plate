4dbec4c806b698d577e679d59de548131aaa0f8b
remusris <remus.risnoveanu@gmail.com>
2024-12-17 00:31:09 -0700

    react flow working

--- entrypoints/example-ui.content/App.tsx
+++ entrypoints/example-ui.content/App.tsx
@@ -1,13 +1,32 @@
 import './style.css';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
 
 function App() {
-  return (
-    <div>
-      <button className="fixed bottom-5 right-5 rounded-md px-4 py-2 bg-blue-500 text-white hover:bg-blue-600">
-        Click me
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            {/* <button className="fixed bottom-5 right-5 rounded-md px-4 py-2 bg-blue-500 text-white hover:bg-blue-600">
+                Click me
+            </button> */}
+
+            <Dialog>
+                <DialogTrigger asChild>
+                    <button className="fixed bottom-5 right-5 rounded-md px-4 py-2 bg-blue-500 text-white hover:bg-blue-600">
+                        Click me
+                    </button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ entrypoints/sidepanel/ColorSelectorNode.tsx
@@ -0,0 +1,38 @@
+import React, { memo } from 'react';
+import { Handle, Position } from '@xyflow/react';
+ 
+const ColorSelectorNode = memo(({ data, isConnectable }) => {
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Left}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={isConnectable}
+      />
+      <div>
+        Custom Color Picker Node: <strong>{data.color}</strong>
+      </div>
+      <input
+        className="nodrag"
+        type="color"
+        onChange={data.onChange}
+        defaultValue={data.color}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="a"
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="b"
+        isConnectable={isConnectable}
+      />
+    </>
+  );
+});
+
+export default ColorSelectorNode;
